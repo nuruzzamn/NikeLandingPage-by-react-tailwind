5b38d54b2276117069f3fa2a76877155ca6dd853
unknown <nur.mnzaman@gmail.com>
2023-09-10 20:16:24 +0600

    [feat] about-us section

--- src/components/SuperQuality.jsx
+++ src/components/SuperQuality.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import Button from "./Button/Button";
+import { shoe8 } from "../assets/images";
 
 const SuperQuality = () => {
   return (
-    <div>SuperQuality</div>
-  )
-}
+    <div id="about-us" className="max-container max-sm:mt-0 flex justify-center items-center max-lg:block max-sm:w-full">
+      <section className="flex flex-col flex-1 justify-start gap-5 lg:mt-[-50px] ">
+        <h2 className="text-4xl font-semibold py-[-10px] ">
+          We Provide You <br className="max-lg:block hidden" />{" "}
+          <span className="font-palanquin text-coral-red">
+            {" "}
+            Super <br className="max-lg:hidden block" /> Quality{" "}
+          </span>
+          Shoes{" "}
+        </h2>
+        <p className="font-montserrat text-xl leading-normal text-gray-500 mt-2 lg:max-w-lg">
+          Ensuring premium comfort and style, our meticulously crafted footwear
+          is designed to elevate your experience, providing you with unmatched
+          quality, innovation, and a touch of elegance.
+        </p>
 
-export default SuperQuality
+        <p className="font-montserrat text-xl leading-normal text-gray-500 mt-2 lg:max-w-lg">
+          Our dedication to detail and excellence ensures your satisfaction
+        </p>
+
+        <section className="mt-8 ">
+          <Button label={"View details"} />
+        </section>
+      </section>
+
+      <section className="flex flex-1 max-lg:mt-16">
+        <img src={shoe8} alt="" />
+      </section>
+    </div>
+  );
+};
+
+export default SuperQuality;
